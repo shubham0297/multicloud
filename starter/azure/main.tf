f06731a45848db21db498f86467e22bdaa6f5cc3
Ubuntu <azureuser@skvm.osxi1fabhy2e1hxo0ptb1u1gid.dx.internal.cloudapp.net>
2023-03-03 02:48:56 +0000

    Azure Terraform changes

--- starter/azure/main.tf
+++ starter/azure/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_container_group" "udacity" {
   location            = data.azurerm_resource_group.udacity.location
   resource_group_name = data.azurerm_resource_group.udacity.name
   ip_address_type     = "Public"
-  dns_name_label      = "udacity-tscotto-azure"
+  dns_name_label      = "udacity-sk-azure"
   os_type             = "Linux"
 
   container {
@@ -16,8 +16,8 @@ resource "azurerm_container_group" "udacity" {
     cpu    = "0.5"
     memory = "1.5"
     environment_variables = {
-      "AWS_S3_BUCKET"       = "udacity-tscotto-aws-s3-bucket",
-      "AWS_DYNAMO_INSTANCE" = "udacity-tscotto-aws-dynamodb"
+      "AWS_S3_BUCKET"       = "udacity-sk-aws-s3-bucket",
+      "AWS_DYNAMO_INSTANCE" = "udacity-sk-aws-dynamodb"
     }
     ports {
       port     = 3000
@@ -30,3 +30,14 @@ resource "azurerm_container_group" "udacity" {
 }
 
 ####### Your Additions Will Start Here ######
+resource "azurerm_mssql_server" "mssql" {
+  name                         = "udacity-sk-azure-sql"
+  resource_group_name          = data.azurerm_resource_group.udacity.name
+  location                     = data.azurerm_resource_group.udacity.location
+  version                      = "12.0"
+
+  tags = {
+    environment = "udacity"
+  }
+}
+
